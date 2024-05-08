55e31ae290341468d5337040bdb444fdc8c8fc0e
Maxim Davydov <maxim.davydov.94@gmail.com>
2024-05-08 17:55:42 +0300

    Add asset prefix for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      src: '/src',
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const basePrefix = env.VITE_BASE_PREFIX;
+
+  const config = {
+    base: '/src',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        src: '/src',
+      },
     },
-  },
+  };
+
+  if (basePrefix) {
+    config.base = basePrefix;
+  }
+
+  return config;
 });
